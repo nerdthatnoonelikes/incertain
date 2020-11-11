b57601ca9c9658c168c509daebac7a07bb568f86
nerdthatnoonelikes <kenrag24@gmail.com>
2020-11-11 16:03:23 -0600

    try to make the lexer better

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,28 @@ const TOKEN_ADD = "ADD";
 const TOKEN_SUBTRACT = "SUBTRACT";
 
 const lexer = (filename: string) => {
+
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
             return console.log(`Error while reading file | Error: ${err}`);
         }
         let tokens = [];
-        for (let x of data) {
-            if (x === "+") {
-                x = `${x}:${TOKEN_ADD}`;
-               
-            } else if("0123456789".indexOf(x) >= 0) {
-                x = `${x}:${TOKEN_INT}`
-            } else if(x === "-") {
-                x = `${x}:${TOKEN_SUBTRACT}`
-            }
-            tokens.push(x);
+       let source = data
+       let index = 0;
+       
+       while (index < source.length) {
+           let current = source[index];
+           if (current === "+") {
+            tokens.push(TOKEN_ADD);
+           } else if(current === "-") {
+            tokens.push(TOKEN_SUBTRACT);
+           } else if("0123456789".indexOf(current) >= 0) {
+            tokens.push(TOKEN_INT);
+           }
+           index++
         }
         console.log(tokens);
-    });
+   });
 }
 
 lexer("test/test.ic")
